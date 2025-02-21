72699433c7c3bd89a5e0be82cd603b098bf9cfd5
Shubham yadav <151907627+shubhamyadav2110@users.noreply.github.com>
2025-02-21 09:51:57 +0000

    commit

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -2,5 +2,27 @@
 
 //alert(3+3) we are using node js not browser
 
-console.log(6); 
+console.log(6
+    +6);//code visibilty must be high 
 console.log("shubham")
+
+let name = "shubham"
+let age = 25
+let isLogedIn = false
+
+
+/* primitive data types
+//number =>2 to power 53
+//bigint => bigger than number
+//string =>" and ''
+//boolean => true/false
+//null => standalone value which is object type
+//undefined => dont have any value
+//null => this is not undefined because having null value
+//symbol =>unique values
+*/
+/* object
+*/
+console.log(typeof(isLogedIn));// boolean type
+console.log(typeof(null));// object type
+console.log(typeof(undefined));// undefined type
