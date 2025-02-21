ec48c0a4ba7306977701c8ab4d86080623cb0647
Shubham yadav <151907627+shubhamyadav2110@users.noreply.github.com>
2025-02-21 09:28:18 +0000

    commit

---
+++ basics/datatypes.js
@@ -0,0 +1,6 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) we are using node js not browser
+
+console.log(6); 
+console.log("shubham")
---
+++ basics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "shubhamyadav2110@gmail.com" //variable declearation
+var accountPassword = "12345" // variable decleration
+acccountCity = "Raipur" // dont use this method 
+
+let accountState;
+// accountId = 2
+accountEmail ="shhubhuyadu5648@gmail.com"
+accountPassword = "788945"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+/*
+prefer not to use var because of issue in block scope and funtional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,acccountCity,accountState]);
