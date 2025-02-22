71609b79b57c1f86fec044d10f0aefb41f4afa20
Shubham yadav <151907627+shubhamyadav2110@users.noreply.github.com>
2025-02-22 10:46:09 +0000

    shu

---
+++ basics/numsmath.js
@@ -0,0 +1,31 @@
+/*const score = 400
+
+console.log(score);
+console.log(typeof(score));
+
+const balance = new Number(100)
+console.log(balance);
+console.log(typeof(balance));
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.89554
+console.log(otherNumber.toPrecision(3));
+
+const hunred = 10000000
+console.log(hunred.toLocaleString());
+console.log(hunred.toLocaleString('en-IN'));
+*/
+console.log(Math);
+console.log(Math.sin(90));
+console.log(Math.abs(4));
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+console.log(Math.round(4.2));
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(2));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
