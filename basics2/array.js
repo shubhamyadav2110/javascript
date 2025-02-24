ad0bde4dcb7f810b3165683737aa4812dda35ec4
Shubham yadav <151907627+shubhamyadav2110@users.noreply.github.com>
2025-02-24 06:32:05 +0000

    commit

---
+++ basics2/array.js
@@ -0,0 +1,30 @@
+//Array
+
+const myArr = [1,2,3,4,5]
+/*console.log(myArr[0]);
+console.log(myArr[1]);
+console.log(myArr[2]);
+console.log(myArr[3]);
+console.log(myArr[4]);
+
+const myArr2 =["shubham","yadav"]
+console.log(myArr2[0]);
+console.log(myArr2[1]);
+
+const myArr3 = new Array(456,456,45652)
+console.log(myArr3[0]);
+console.log(myArr3[1]);
+console.log(myArr3[2]);
+
+//Array methods
+
+//myArr.push("dj");
+//myArr.push(1,"djj")*/
+myArr.unshift(9)//add the 9 in intial of array
+console.log(myArr);
+
+myArr.shift()//thi will remove the first element of array
+console.log(myArr);
+
+console.log(myArr.includes(9));//(false) this will check that 9 exist in array or not 
+
